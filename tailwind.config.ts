a2dd7b7a8175ed81a7eecb943a9c6ca9dbe24796
moonhee0507 <sjmh0507@naver.com>
2023-08-26 17:10:11 +0900

    :lipstick: setting design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,11 +9,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "ds-dark1": "#151619",
+        "ds-dark2": "#1d1f22",
+        "ds-dark3": "#2b2d31",
+        "ds=dark4": "#35393f",
+        "ds-gray1": "#5a6069",
+        "ds-gray2": "#7c8187",
+        "ds-gray3": "#c1c4cb",
+        "ds-gray4": "#e4e4e4",
+        "ds-gray5": "#f5f5f5",
+        "ds-primary": "#e46643",
+        "ds-primary-hover": "#f39765"
       },
+      backgroundImage: {
+        
+      },
+      screens: {
+        desktop: "1440px"
+      }
     },
   },
   plugins: [],
